7e8423ea6f7edf4c75b485f03b84f4782ee267f5
salah nahed <salahnahed@icloud.com>
2021-03-05 02:00:43 +0200

    Stretchy Header

--- Stretchy Header/ViewController.swift
+++ Stretchy Header/ViewController.swift
@@ -7,13 +7,166 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let padding: CGFloat = 16
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
+    
+    private func setupCollectionView() {
+        if let layout = collectionViewLayout as? CustomPDPLayout {
+            layout.minimumLineSpacing = 8
+            layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+            layout.onUpdateLayoutAttributesForSectionHeader = updateHeaderViewLayout()
+        }
+        
+        collectionView.register(AttributeCell.self, forCellWithReuseIdentifier: AttributeCell.cellId)
+        collectionView.register(PDPHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: PDPHeaderView.headerId)
+    }
+    
 
-
+    private func updateHeaderViewLayout () -> CustomPDPLayout.UpdateLayoutAttributesObserver {
+        { [weak self] in
+            guard let self = self else { return }
+            let firstSection = IndexPath(item: .zero, section: .zero)
+            guard let headerView = self.collectionView.supplementaryView(forElementKind: UICollectionView.elementKindSectionHeader, at: firstSection) as? PDPHeaderView else { return }
+            headerView.collectionView.collectionViewLayout.invalidateLayout()
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AttributeCell.cellId, for: indexPath)
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: PDPHeaderView.headerId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        .init(width: collectionView.frame.width - 2 * padding, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        .init(width: collectionView.frame.width, height: 250)
+    }
 }
 
+class AttributeCell: UICollectionViewCell {
+    
+    static let cellId = "attributeCellId"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class PDPHeaderView: UICollectionReusableView {
+    
+    static let headerId = "PDPHeaderViewId"
+    
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let view = UICollectionView(frame: frame, collectionViewLayout: layout)
+        view.isPagingEnabled = true
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(AttributeCell.self, forCellWithReuseIdentifier: AttributeCell.cellId)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PDPHeaderView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AttributeCell.cellId, for: indexPath)
+        cell.backgroundColor = UIColor(hue: CGFloat(drand48()), saturation: 1, brightness: 1, alpha: 1)
+        return cell
+    }
+}
+
+extension PDPHeaderView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        collectionView.frame.size
+    }
+}
+
+
+class CustomPDPLayout: UICollectionViewFlowLayout {
+    
+    typealias UpdateLayoutAttributesObserver = (() -> Void)
+    
+    var onUpdateLayoutAttributesForSectionHeader: UpdateLayoutAttributesObserver?
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let layoutAttributes = super.layoutAttributesForElements(in: rect),
+              let collectionView = collectionView else { return nil }
+        layoutAttributes.forEach({ (attribute) in
+            if attribute.representedElementKind == UICollectionView.elementKindSectionHeader {
+                let contentOffsetY = collectionView.contentOffset.y
+                if contentOffsetY < 0 {
+                    let width = collectionView.frame.width
+                    // as contentOffsetY is -ve, the height will increase based on contentOffsetY
+                    let height = attribute.frame.height - contentOffsetY
+                    attribute.frame = CGRect(x: 0, y: contentOffsetY, width: width, height: height)
+                }
+                onUpdateLayoutAttributesForSectionHeader?()
+            }
+        })
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        true
+    }
+}
